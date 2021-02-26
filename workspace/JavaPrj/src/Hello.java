a94afb603a0d1238e74a903e049681110f10a975
unknown <aamj0226@gmail.com>
2021-02-26 21:31:17 +0900

    add: write, print, println, printf 함수

--- workspace/JavaPrj/src/Hello.java
+++ workspace/JavaPrj/src/Hello.java
@@ -3,28 +3,41 @@ class Hello {
         int kor, eng; 
         int math; 
         int total;
-        double avg;
+        float avg;
 
         kor = 90;
         eng = 100;
-        math = 95;
+        math = 97;
 
         total = kor + eng + math;
-        avg = total / 3;
+        avg = total / 3.0f;
 
         //System.out.printf("total is %d\n", total);
         
-        System.out.print("┌───────────────────────────┐\n");
-        System.out.print("│          성적 출력          │\n");
-        System.out.print("└───────────────────────────┘\n");
+        System.out.println("┌───────────────────────────┐");
+        System.out.println("│          성적 출력          │");
+        System.out.println("└───────────────────────────┘");
         
-        System.out.print("국어1 : 0\n");
-        System.out.print("국어2 : 0\n");
-        System.out.print("국어3 : 0\n");
-        System.out.print("총점 : 0\n");
-        System.out.print("평균 : 0\n");
+        System.out.printf("국어 : %d\n", kor);
+        System.out.printf("영어 : %d\n", eng);
+        System.out.printf("수학 : %d\n", math);
+        
+        System.out.printf("총점 : %d\n", total);
+        System.out.printf("평균 : %3.2f\n", avg);
+        
+        System.out.write(51);
+        
+  
 
-        System.out.print("─────────────────────────────\n");
+       
+        
+        System.out.print("A"); 
+
+
+
+      
+        //System.out.printf("%1$d, %2$d, %1$d, %2$d, %3$d\n", 1, 2, 5);
+
+        System.out.println("─────────────────────────────");
     }
 }
-
