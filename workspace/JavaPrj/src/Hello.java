83870fa5e6c079d333c8a41991529ef416034ddb
unknown <aamj0226@gmail.com>
2021-03-02 14:47:02 +0900

    add: 성적 입력 및 파일 입출력 코드 추가

--- workspace/JavaPrj/src/Hello.java
+++ workspace/JavaPrj/src/Hello.java
@@ -1,43 +1,56 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
 class Hello {
-    public static void main(String[] args) {
-        int kor, eng; 
-        int math; 
+    public static void main(String[] args) throws IOException {
+        
+    	// 기본 형식
+        int kor, eng, math;
         int total;
         float avg;
+        
+        // 성적 입력
+        System.out.println("┌───────────────────────────┐");
+        System.out.println("│          성적 입력          │");
+        System.out.println("└───────────────────────────┘");
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.printf("국어: ");
+        kor = scan.nextInt();
 
-        kor = 90;
-        eng = 100;
-        math = 97;
+        System.out.printf("영어: ");
+        eng = scan.nextInt();
+        
+        System.out.printf("수학: ");
+        math = scan.nextInt();
 
         total = kor + eng + math;
         avg = total / 3.0f;
 
-        //System.out.printf("total is %d\n", total);
-        
+        // 성적 출력
         System.out.println("┌───────────────────────────┐");
         System.out.println("│          성적 출력          │");
         System.out.println("└───────────────────────────┘");
-        
+
         System.out.printf("국어 : %d\n", kor);
         System.out.printf("영어 : %d\n", eng);
         System.out.printf("수학 : %d\n", math);
         
         System.out.printf("총점 : %d\n", total);
-        System.out.printf("평균 : %3.2f\n", avg);
-        
-        System.out.write(51);
-        
-  
-
-       
-        
-        System.out.print("A"); 
-
-
-
-      
-        //System.out.printf("%1$d, %2$d, %1$d, %2$d, %3$d\n", 1, 2, 5);
+        System.out.printf("평균 : %5.2f\n", avg);
 
         System.out.println("─────────────────────────────");
+        
+        // 파일 입출력
+        FileInputStream fis = new FileInputStream("C:\\Users\\이민주\\Desktop\\coding\\2021-02\\workspace\\JavaPrj\\data.txt");
+        Scanner scan1 = new Scanner(fis);
+        kor = scan1.nextInt(); 
+        eng = scan1.nextInt(); 
+        math = scan1.nextInt();
+        System.out.printf("국어 : %d\n", kor);
+        System.out.printf("영어 : %d\n", eng);
+        System.out.printf("수학 : %d\n", math);
+        
     }
 }
