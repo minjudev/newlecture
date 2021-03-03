6fa7e5dcf2b3abc98ae1e1108f84eeab5a314051
unknown <aamj0226@gmail.com>
2021-03-03 14:39:20 +0900

    add: 파일 입출력 코드 추가

--- workspace/JavaPrj/src/Hello.java
+++ workspace/JavaPrj/src/Hello.java
@@ -1,5 +1,7 @@
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 class Hello {
@@ -9,6 +11,7 @@ class Hello {
         int kor, eng, math;
         int total;
         float avg;
+        
         
         // 성적 입력
         System.out.println("┌───────────────────────────┐");
@@ -27,6 +30,7 @@ class Hello {
 
         total = kor + eng + math;
         avg = total / 3.0f;
+        
 
         // 성적 출력
         System.out.println("┌───────────────────────────┐");
@@ -42,15 +46,27 @@ class Hello {
 
         System.out.println("─────────────────────────────");
         
-        // 파일 입출력
+        
+        // 파일 입력
+        // data.txt 파일에서 국어, 영어, 수학 성적 입력 받기
         FileInputStream fis = new FileInputStream("C:\\Users\\이민주\\Desktop\\coding\\2021-02\\workspace\\JavaPrj\\data.txt");
         Scanner scan1 = new Scanner(fis);
         kor = scan1.nextInt(); 
         eng = scan1.nextInt(); 
         math = scan1.nextInt();
-        System.out.printf("국어 : %d\n", kor);
-        System.out.printf("영어 : %d\n", eng);
-        System.out.printf("수학 : %d\n", math);
+        scan1.close(); // scanner에게 먼저 닫아준다고 알려줘야 함
+        fis.close(); // 그 후에 파일을 닫아줌
         
+        
+        // 파일 출력
+        FileOutputStream fos = new FileOutputStream("C:\\Users\\이민주\\Desktop\\coding\\2021-02\\workspace\\JavaPrj\\data.txt");
+        PrintStream fout = new PrintStream(fos); // system.out은 콘솔에 출력을 하는 역할
+        kor++; 
+        eng--; 
+        
+        fout.printf("%d %d %d", kor, eng, math); // Printstream 안에 system.out이 있음
+        fout.close();
+        fos.close();
+                
     }
 }
---
+++ workspace/JavaPrj/src/ex01/bit/Program.java
@@ -0,0 +1,14 @@
+package ex01.bit;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Program {
+
+	public static void main(String[] args) throws IOException {
+
+		FileInputStream fis = new FileInputStream("C:\\Users\\¿ÃπŒ¡÷\\Desktop\\coding\\2021-02\\workspace\\JavaPrj\\image.bmp");
+		
+	}
+
+}
