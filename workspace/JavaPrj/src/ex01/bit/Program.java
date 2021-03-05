ccd53a85dac71a5f472ff32405c1074e4ee1f395
unknown <aamj0226@gmail.com>
2021-03-05 15:51:02 +0900

    update: 비트 논리 연산자 코드 수정

--- workspace/JavaPrj/src/ex01/bit/Program.java
+++ workspace/JavaPrj/src/ex01/bit/Program.java
@@ -7,8 +7,40 @@ public class Program {
 
 	public static void main(String[] args) throws IOException {
 
-		FileInputStream fis = new FileInputStream("C:\\Users\\이민주\\Desktop\\coding\\2021-02\\workspace\\JavaPrj\\image.bmp");
+		// 이미지 파일을 읽기 위한 파일 입력 스트림
+		FileInputStream fis = new FileInputStream("res/image.bmp");
 		
+		// read()는 1바이트씩 읽음
+		// 파일 구조 상에서 signature를 읽어야 이미지의 파일 크기를 읽을 수 있음
+		int b; // 2바이트
+		b = fis.read();
+		b = fis.read();
+		
+		// fileSize
+		int b1 = fis.read(); 
+		int b2 = fis.read(); 
+		int b3 = fis.read();
+		int b4 = fis.read();
+		
+		int fileSize = 0; 
+//		fileSize = b1; // [][][][b1];
+//		fileSize = b2; // [][][][b2];
+//		fileSize = b3; // [][][][b3];
+//		fileSize = b4; // [][][][b4];
+		
+		fileSize = (b4 & 0x000000ff) << 8*3 | // [b4][][][];
+				   (b3 & 0x000000ff) << 8*2 | // [b4][b3][][];
+		           (b2 & 0x000000ff) << 8*1 | // [b4][b3][b2][];
+		           (b1 & 0x000000ff);		  // [b4][b3][b2][b1];
+				
+		System.out.println(Integer.toBinaryString(b4));
+		System.out.println(Integer.toBinaryString(b3));
+		System.out.println(Integer.toBinaryString(b2));
+		System.out.println(Integer.toBinaryString(b1));
+		
+		System.out.printf("file size: %d\n", fileSize);	
+		
+		fis.close();
 	}
 
 }
