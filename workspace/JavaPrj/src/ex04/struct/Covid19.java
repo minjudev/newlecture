a2082189a5790f5a5dd691fc4ef96499d52d2c20
unknown <aamj0226@gmail.com>
2021-03-19 23:24:46 +0900

    update: Covid19 구조체 변수 추가 및 이름 수정

--- workspace/JavaPrj/src/ex04/struct/Covid19.java
+++ workspace/JavaPrj/src/ex04/struct/Covid19.java
@@ -1,12 +1,18 @@
 package ex04.struct;
 
 public class Covid19 {
-	// 사용자 자료형: 사용자가 만든 자료형
-	// Covid19 class를 만들면 Covid19라는 큰 그릇(String date, int positive, int death를 담을 수 있는 그릇)이 메모리 상에 준비가 되는 것음
+	// 사용자 정의 자료형: 사용자가 만든 자료형
+	// Covid19 class를 만들면 Covid19라는 큰 그릇(String date, int cases, int dead를 담을 수 있는 그릇)이 메모리 상에 준비가 되는 것음
 
 	 String date;
-	 int positive;
+	 int cases;
 	 int dead;
 	 int tests;
-
+	 int selfIsolation; // Camel case
+	 int release;
+	 int recover;
+	 int selfIsolationDomestic;
+	 int selfIsolationOverseas;
+	 String pubDate;
+	 
 }
