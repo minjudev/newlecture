bc32b70443b4dfbbdfe80ae84e1ee152886ee24a
unknown <aamj0226@gmail.com>
2021-04-12 23:19:43 +0900

    add: 인터페이스(중첩 클래스, 익명 클래스) 예제

--- workspace/JavaPrj/src/ex09/인터페이스/exam/ybm/Program.java
+++ workspace/JavaPrj/src/ex09/인터페이스/exam/ybm/Program.java
@@ -4,13 +4,39 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		YBMExam exam = new YBMExam(1,1,1,1,1);
+		YBMExam exam = new YBMExam();
 		YBMExamConsole console = new YBMExamConsole();
 		console.setExam(exam);
-		console.setBanner(console);
+		
+		// import를 하지 않으면 XXX 앞에 아우터 클래스(YBMExamConsole)의 이름을 명시해주어야 함
+		// 이너 클래스는 아우터 클래스를 통해서 만들어져야 함
+		// 객체가 가지고 있는 new라는 연산자를 사용해서 이너 클래스를 만들자
+		// YBMExamConsole.XXX x = console.new XXX();
+		
+		// setBanner에 꽂아넣게 될 클래스가 아우터 클래스 안에 있는 이너 클래스임
+		// console.setBanner(x);
 		
 		console.input();
 		console.print();
 	}
 
 }
+
+/*
+ * 인터페이스: 사물 또는 공간을 이어주는 접점
+ * 자바 인터페이스: 객체와 객체를 이어주는 접점 
+ * 
+ * 인터페이스를 통해서 정의하고 구현하고 사용하는 절차가 접합이 되면?
+ * 다형성이 발생
+ * 
+ * 일부 절차가 인터페이스로 정의된다?
+ * A 클래스가 인터페이스를 정의하였다.
+ * 
+ * 인터페이스를 구현하는 방법을 4가지로 나누어볼 수 있다.
+ * 1. 외부 클래스로 구현
+ * 2. 내부 클래스로 구현
+ * 3. 메소드 내의 중첩 클래스로 구현
+ * 4. 익명 클래스로 구현
+ * 5. 람다식을 이용한 구현 
+ * 
+*/
--- workspace/JavaPrj/src/ex09/인터페이스/exam/ybm/YBMExamConsole.java
+++ workspace/JavaPrj/src/ex09/인터페이스/exam/ybm/YBMExamConsole.java
@@ -5,7 +5,77 @@ import java.util.Scanner;
 import ex09.인터페이스.exam.Banner;
 import ex09.인터페이스.exam.ExamConsole;
 
-public class YBMExamConsole extends ExamConsole implements Banner {
+// 다중 상속의 문제점(is a 상속 + 인터페이스 구현): 부모 클래스의 메소드를 오버라이드하게 됨
+public class YBMExamConsole extends ExamConsole /*implements Banner*/ {
+	
+	private int a;
+	
+	public YBMExamConsole() {
+//		class XXX implements Banner {
+//			private int a;
+//			
+//			public XXX() {
+//				a = 4;
+//			}
+//			
+//			@Override
+//			public void print() {
+//				// YBMExamConsole의 멤버변수를 사용 가능
+//				// a = 3;
+//			
+//				System.out.println("----------------------------------");
+//				System.out.println("            YBM 성적 출력           ");
+//				System.out.println("----------------------------------");
+//				System.out.println(this.a); // 4가 출력
+//				
+//				// 아우터 클래스의 a 값을 사용하고 싶을 때는?
+//				System.out.println(YBMExamConsole.this.a); // 3이 출력
+//			}
+//			
+//			@Override
+//			public void printEnd() {
+//				System.out.println("----------------------------------");		
+//			}
+//		}
+//		
+		
+		// 중첩된 클래스를 정의할 때 new 앞에 this가 생략되어있다는 것을 인지하고 있기
+		// 독립적인 클래스를 생성하는 것과 모양은 비슷하지만, 중첩 클래스를 생성하고 있다는 것 인지하기
+//		/*YBMExamConsole.*/XXX x = /*this.*/new XXX();	
+		
+		// 인터페이스를 구현하는 것이 목적이면 클래스명을 정하지 말자는 의도에서 anonymous class가 나옴
+		// 위에서 x라는 클래스를 만드는 것과 달리 클래스명을 거론하지 말자
+//		/*this.*/setBanner(x);
+
+		// 인터페이스를 구현하는 것이 목적이면 클래스를 따로 정의하지 말고 메소드 안에서 클래스를 정의하자
+		// 메소드 안에서 정의된 클래스 - 다른 곳에서는 절대 볼 수 없음
+		// 위 메소드(YBMExamConsole 생성자)를 호출할 때 클래스가 한 번만 사용되었다가 끝나게 하기
+		
+		// 이렇게 할 거면 클래스명 자체를 없애는 게 어떨까 -> 익명 클래스 등장
+		// 인터페이스가 코드를 구현하는 작업을 하면서 new 하게 하기
+		// 인터페이스를 실제로 구현한 무명의 클래스를 new 하게 해서, 그걸 아래 setBanner()에서 세팅할 수 있게 됨
+		Banner banner = new Banner() {
+				
+			@Override
+			public void print() {
+				a = 3;
+				System.out.println("----------------------------------");
+				System.out.println("            YBM 성적 출력           ");
+				System.out.println("----------------------------------");
+				
+				// 아우터 클래스의 a 값을 사용하고 싶을 때는?
+				System.out.println(YBMExamConsole.this.a); // 3이 출력
+			}
+			
+			@Override
+			public void printEnd() {
+				System.out.println("----------------------------------");		
+			}
+			
+		};
+		
+		setBanner(banner);
+	}
 	
 	@Override
 	protected void onPrint() {
@@ -37,16 +107,33 @@ public class YBMExamConsole extends ExamConsole implements Banner {
 		exam.setAa(aa);
 	}
 
-	@Override
-	public void printStart() {
-		System.out.println("----------------------------------");
-		System.out.println("            YBM 성적 출력           ");
-		System.out.println("----------------------------------");
-	}
-
-	@Override
-	public void printEnd() {
-		System.out.println("----------------------------------");		
-	}
+	// Banner의 기능을 구현하려 했으나 ExamConsole의 print를 재정의하게 됨
+	// 해결 방법: nested class
+//	public class XXX implements Banner {
+//		private int a;
+//		
+//		public XXX() {
+//			a = 4;
+//		}
+//		
+//		@Override
+//		public void print() {
+//			// YBMExamConsole의 멤버변수를 사용 가능
+//			// a = 3;
+//		
+//			System.out.println("----------------------------------");
+//			System.out.println("            YBM 성적 출력           ");
+//			System.out.println("----------------------------------");
+//			System.out.println(this.a); // 4가 출력
+//			
+//			// 아우터 클래스의 a 값을 사용하고 싶을 때는?
+//			System.out.println(YBMExamConsole.this.a); // 3이 출력
+//		}
+//		
+//		@Override
+//		public void printEnd() {
+//			System.out.println("----------------------------------");		
+//		}
+//	}
 	
 }
